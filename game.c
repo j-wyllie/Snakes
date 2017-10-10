711a8043f9c9fefdf2e3974c9526c90fc0f73500
Joshua William Wyllie <jwy31@cs16064ho.canterbury.ac.nz>
2017-10-10 16:46:53 +1300

    add framework code

--- game.c
+++ game.c
@@ -1,14 +1,176 @@
 #include "system.h"
+#include "tinygl.h"
+#include "task.h"
+#include "navswitch.h"
+#include "led.h"
+#include "../fonts/font3x5_1.h"
+
+
+enum {DISPLAY_UPDATE_RATE = 500};
+enum {FLASHER_UPDATE_RATE = 500};
+enum {BUTTON_POLL_RATE = 100};
+enum {GAME_UPDATE_RATE = 100};
+
+typedef enum {STATE_DUMMY, STATE_INIT, STATE_START,
+              STATE_PLAYING, STATE_OVER,
+              STATE_READY} state_t;
+
+static state_t state = STATE_INIT;
+static uint8_t display[TINYGL_WIDTH * TINYGL_HEIGHT];
+
+
+static void game_init(void)
+{
+    // display_init
+    tinygl_init (DISPLAY_UPDATE_RATE);
+    tinygl_font_set (&font3x5_1);
+    tinygl_text_mode_set (TINYGL_TEXT_MODE_SCROLL);
+    tinygl_text_dir_set (TINYGL_TEXT_DIR_ROTATE);
+    tinygl_text_speed_set (10);
+
+    // navswitch init
+    navswitch_init ();
+
+    // game init
+}
+
+static void display_task(void *data)
+{
+    if (state == STATE_PLAYING) {
+        /* Update display.  */
+        for (j = 0; j < TINYGL_HEIGHT; j++)
+            for (i = 0; i < TINYGL_WIDTH; i++)
+            {
+                tinygl_point_t point = {i, j};
+                tinygl_draw_point (point, display[i][j]);
+            }
+    }
+
+    tinygl_update ();
+}
+
+static void navswitch_task(void *data)
+{
+    navswitch_update ();
+
+    if (navswitch_push_event_p (NAVSWITCH_EAST))
+    {
+        switch (state)
+        {
+        case STATE_READY:
+            break;
+
+        case STATE_PLAYING:
+            break;
+
+        default:
+            break;
+        }
+    }
+
+    if (navswitch_push_event_p (NAVSWITCH_WEST))
+    {
+        switch (state)
+        {
+        case STATE_READY:
+            break;
+
+        case STATE_PLAYING:
+            break;
+
+        default:
+            break;
+        }
+    }
+
+    if (navswitch_push_event_p (NAVSWITCH_WEST))
+    {
+        switch (state)
+        {
+        case STATE_READY:
+            break;
+
+        case STATE_PLAYING:
+            break;
+
+        default:
+            break;
+        }
+    }
+
+    if (navswitch_push_event_p (NAVSWITCH_PUSH))
+    {
+        switch (state)
+        {
+        case STATE_READY:
+            state = STATE_START;
+            break;
+
+        case STATE_PLAYING:
+            break;
+
+        default:
+            break;
+        }
+    }
+}
+
+static void game_task(void *data)
+{
+    switch (state)
+    {
+    case STATE_PLAYING:
+        if (! spacey_update ())
+        {
+            game_over_display (message);
+            game_over_ticks = 0;
+            led_set (LED1, 1);
+            state = STATE_OVER;
+        }
+        break;
+
+    case STATE_INIT:
+        break;
+
+    case STATE_OVER:
+
+    case STATE_READY:
+
+    case STATE_START:
+        /* Turn that bloody blimey space invader off...  */
+        game_start (&game_data);
+        led_set (LED1, 0);
+        state = STATE_PLAYING;
+        break;
+    }
+}
 
 int main (void)
 {
+    game_init();
+
+    task_t tasks[] =
+    {
+        {
+            .func = display_task,
+            .period = TASK_RATE / DISPLAY_UPDATE_RATE,
+            .data = 0
+        },
+        {
+            .func = navswitch_task,
+            .period = TASK_RATE / BUTTON_POLL_RATE,
+            .data = 0
+        },
+        {
+            .func = game_task,
+            .period = TASK_RATE / GAME_UPDATE_RATE,
+            .data = 0
+        },
+    };
+
+
     system_init ();
 
-
-    while (1)
-    {
-
-
-
-    }
+    task_schedule (tasks, ARRAY_SIZE (tasks));
+    return 0;
 }
